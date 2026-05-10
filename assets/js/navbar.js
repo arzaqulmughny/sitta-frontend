29744d5537a7607bdbd084c5b23d4b0a4d638cfd
arzaqulmughny <zaarza03@gmail.com>
2026-05-10 20:04:02 +0700

    feat: update navigation menu with routing, new branding, and logout confirmation logic

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,4 +1,16 @@
-const toggleNavbar = () => {
+const toggleNavbar = (event) => {
+    event.stopPropagation();
+    
     const menu = document.getElementById('navbar-menu');
     menu.classList.toggle('navbar-menu--active')
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Set active menu navbar
+    const pathname = window.location.pathname;
+    document.querySelectorAll('.navbar-links').forEach((link) => {
+        if (link.pathname === pathname) {
+            link.classList.add('navbar-links--active')
+        }
+    })
+})
